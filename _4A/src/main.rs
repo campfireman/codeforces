ae3addf3dd7d19dcb005893edeb6898dc2c6ae08
Ture <ture.cl.business@gmail.com>
2021-09-25 17:11:45 +0200

    Solve 4A

---
+++ _4A/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Failed to read line.");
+    let n: u32 = n.trim().parse().expect("Input not an integer.");
+    if n == 2 {
+        println!("NO");
+        return
+    }
+    if n % 2 == 0 {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
+
+}
